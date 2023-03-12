d43b30f7d63788e3bb2e6da49cb073fbdd37247d
Denys Denysiuk <denya5111@gmail.com>
2023-03-13 00:08:43 +0100

    complite 01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,27 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+function createListItemsMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }) => `<div class="gallery__item">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" onclick="return false" style="display: block"/>
+</a>
+</div>`
+    )
+    .join('');
+}
+
+const galleryContainer = document.querySelector('.gallery');
+
+galleryContainer.innerHTML = createListItemsMarkup(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
