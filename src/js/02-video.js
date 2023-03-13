46379aaf8758236c0ab1cf0729a3028dbc0e8ccd
Denys Denysiuk <denya5111@gmail.com>
2023-03-14 00:14:17 +0100

    homework_8 v1.0

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player
+  .setCurrentTime(localStorage.getItem(STORAGE_KEY))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+
+player.on(
+  'timeupdate',
+  throttle(function (currentTime) {
+    localStorage.setItem(STORAGE_KEY, currentTime.seconds);
+  }, 1000)
+);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('.feedback-form input');
+const textarea = document.querySelector('.feedback-form textarea');
+const submitButton = document.querySelector('.feedback-form button');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+let formData = {};
+
+form.addEventListener('input', throttle(handlerInputData, 500));
+
+function handlerInputData(e) {
+  formData[e.target.name] = e.target.value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+if (localStorage.getItem(STORAGE_KEY)) {
+  formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  input.value = formData.email || '';
+
+  textarea.value = formData.message || '';
+}
+
+submitButton.addEventListener('click', handlerClickSubmitButton);
+
+function handlerClickSubmitButton(e) {
+  e.preventDefault();
+
+  console.log(formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+
+  formData = {};
+
+  form.reset();
+}
